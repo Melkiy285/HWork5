72c1da8cd8aadb780dab6f0df92fac9d568836d5
Melkiy285 <mixaruno@gmail.com>
2024-11-03 19:47:37 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var=(tuple[(0,7,5,True,"apple")])
+print(immutable_var)
+#tuple[0]=1 неизмененный
+mutable_list=[0,7,5,True,"apple"]
+print(mutable_list)
+mutable_list[0]="я тут"
+print(mutable_list)
+
+
+
